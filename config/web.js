02ea82132ff59113dc5db67dc2f39dec1aaf861e
Jam Risser <jam@jamrizzi.com>
2018-01-01 03:21:21 -0600

    Finished main validation logic

--- config/web.js
+++ config/web.js
@@ -1,6 +1,6 @@
-import expressValidator from 'express-validator';
-import _ from 'lodash';
 import boom from 'boom';
+import expressValidator from 'express-validator';
+import validator from '../lib/validator';
 
 export default {
 
@@ -12,21 +12,6 @@ export default {
 
     expressValidator: expressValidator(),
 
-    validator: async (req, res, next) => {
-      const validator = {
-        body: {}
-      };
-      _.each(validator, (rules, key) => {
-        switch(key) {
-          case 'body':
-            req.checkBody(rules);
-            break;
-        }
-      });
-      await req.getValidationResult().then(result => result.throw()).catch((err) => {
-        throw boom.badRequest(err.message, err.array());
-      }).catch(next);
-      return next();
-    }
+    validator
   }
 };
